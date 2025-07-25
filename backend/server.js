f86b4ec84a3503ab0622c3ae2760ce63bd09402a
Puneeth067 <kumarpuneeth631@gmail.com>
2025-07-25 22:10:04 +0530

    frontend vercel orgin added in backend server

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
-    ? ['https://your-frontend-url.vercel.app']
+    ? ['https://https://readix-ten.vercel.app']
     : ['http://localhost:5173'],
   credentials: true
 }));
